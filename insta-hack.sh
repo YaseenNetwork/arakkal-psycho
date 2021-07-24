502fea4e8c21c90f919ad50b7e02ab632c550547
Incredible Hacker <78790759+IncredibleHacker@users.noreply.github.com>
2021-07-24 23:12:48 +0530

    Update insta-hack.sh

--- insta-hack.sh
+++ insta-hack.sh
@@ -10,7 +10,11 @@ Purple="\033[1;35m"      # Purple
 Cyan="\033[1;36m"        # Cyan
 White="\033[1;37m"       # White
 
-
+clear
+apt-get update
+apt-get upgrade
+apt-get install python
+apt-get install python2
 clear
 echo -e "$Purple Created By \e[1;34m"
        figlet Incredible Hacker | lolcat
