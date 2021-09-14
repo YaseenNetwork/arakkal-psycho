83c6a8ac9f783126542bf98bd34737a39ffcd38e
Incredible Hacker <78790759+IncredibleHacker@users.noreply.github.com>
2021-09-14 23:14:30 +0530

    Update ighack.sh

--- ig_brute_force/ighack.sh
+++ ig_brute_force/ighack.sh
@@ -122,7 +122,7 @@ printf "\e[1;92m[\e[0m\e[1;77m8\e[0m\e[1;92m]\e[0m\e[1;93m More\e[0m\n"
 printf "\e[1;92m[\e[0m\e[1;77m9\e[0m\e[1;92m]\e[0m\e[1;93m Exit\e[0m\n"
 read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose an option: \e[0m' option
 if [[ $option == 1 || $option == 01 ]]; then
-cd ig_brute_force/main
+cd $HOME/ig_brute_force/main
 bash auto.sh
 elif [[ $option == 2 || $option == 02 ]]; then
 cd ig_brute_force/main
